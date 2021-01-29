6108de83ff584bfd5dddc68b10f3f70c031d4576
yogeswara-reddy-b <78098006+yogeswara-reddy-b@users.noreply.github.com>
2021-01-29 15:41:11 +0530

    Update settings.py

--- Licenseportalapplication/Licenseportalapplication/settings.py
+++ Licenseportalapplication/Licenseportalapplication/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Licenseportalapplication.wsgi.application'
 
 
-# Databases
+# Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
